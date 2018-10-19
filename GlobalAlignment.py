5bc3a4a2b178e63047e23ec1e56f3c20be22c0a9
Zelin <wangzl1116@mails.jlu.edu.cn>
2018-10-19 18:21:40 +0800

    read data

---
+++ GlobalAlignment.py
@@ -0,0 +1,1 @@
+import ReadData as RD
--- ReadData.py
+++ ReadData.py
@@ -1,33 +1,54 @@
+from typing import Dict
 import numpy as np
 
-Seq = ["",""]
-f = open("Sequence.txt","r")  
-all_lines = f.readlines()
-# 读取序列
-Seq_cnt = -1
-for line in all_lines:
-    if line[0] == '>':
-        Seq_cnt+=1
-        continue
-    Seq[Seq_cnt] += line.strip('\n')
-   
-f.close()
 
-f = open("BLOSUM62.txt","r")
-all_lines = f.readlines() 
-# Matrix 第一行是字母
-Matrix_Cnt = len(all_lines)
-BLOSUM_INDEX = []
-for line in all_lines:
-    if line[0] == '#' :
-        Matrix_Cnt -= 1
-        continue
-    if line[0] == ' ':
-        Matrix_Cnt -= 1
-        BLOSUM_Matrix = np.zeros((Matrix_Cnt, Matrix_Cnt))
-        
-        continue
-    
-    
-print(Matrix_Cnt)
-f.close()
+def Read_Two_Seq(file_name: str = "Sequence.txt") -> list:
+    Seq = ["", ""]
+    f = open(file_name, "r")
+    all_lines = f.readlines()
+    # 读取序列
+    Seq_cnt = -1
+    for line in all_lines:
+        if line[0] == '>':
+            Seq_cnt += 1
+            continue
+        Seq[Seq_cnt] += line.strip('\n')
+    f.close()
+    return Seq
+
+
+def Read_BLO_Matrix(file_name: str = "BLOSUM62.txt") -> np.ndarray and dict:
+    f = open(file_name, "r")
+    all_lines = f.readlines()
+    # Matrix 第一行是字母
+    Matrix_Cnt = len(all_lines)
+    Index = 0
+    BLOSUM_Index_Dic: Dict[str, int] = {}
+    for line in all_lines:
+        if line[0] == '#':
+            Matrix_Cnt -= 1
+            continue
+        elif line[0] == ' ':
+            Matrix_Cnt -= 1
+            BLOSUM_Matrix = np.zeros((Matrix_Cnt, Matrix_Cnt))
+            continue
+        elif line[0].isalpha() or line[0] == '*':
+            # 映射关系建立
+            BLOSUM_Index_Dic[line[0]] = Index
+            # 矩阵建立
+            one_line = line.strip('\n').split()
+            one_line.remove(one_line[0])
+            res = list(map(int, one_line))
+            BLOSUM_Matrix[Index] = res
+            Index += 1
+    f.close()
+    return BLOSUM_Matrix, BLOSUM_Index_Dic
+
+
+def get_Score_between_to_char(a: chr, b: chr) -> int:
+    mat, index = Read_BLO_Matrix()
+    a_index = index[a.upper()]
+    ret_list = mat[a_index]
+    return ret_list[index[b.upper()]]
+
+# print(get_Score_between_to_char('r','r'))
